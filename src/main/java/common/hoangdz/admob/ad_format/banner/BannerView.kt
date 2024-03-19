56646433060a8f8357416c2db47d12bc7feb4004
hoangdv123xyz <hoangdv@wavez.vn>
2024-03-19 11:39:42 +0700

    update wfh

--- src/main/java/common/hoangdz/admob/ad_format/banner/BannerView.kt
+++ src/main/java/common/hoangdz/admob/ad_format/banner/BannerView.kt
@@ -7,10 +7,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.google.android.gms.ads.AdView
 import common.hoangdz.admob.ad_format.AdFormatViewModel
 import common.hoangdz.lib.jetpack_compose.exts.SafeModifier
@@ -26,31 +32,51 @@ fun BannerView(
 ) {
     val owner = LocalLifecycleOwner.current
     val loaderStateCollection by adFormatViewModel.bannerLoaderState.collectWhenResume()
-    if (loaderStateCollection.state != DataResult.DataState.ERROR) {
-        val config = LocalScreenConfigs.current
-        Box {
-            AndroidView(modifier = SafeModifier.fillMaxWidth(), factory = {
-                return@AndroidView AdView(it).also { view ->
-                    adFormatViewModel.loadBanner(
-                        config.route.replace("\\?.*".toRegex(), ""), view, usingCollapsible, owner
-                    )
-                }
-            })
-            if (loaderStateCollection.state == DataResult.DataState.LOADING) {
-                Row {
-                    Box(
-                        modifier = SafeModifier
-                            .size(50.sdp)
-                            .shimmerEffect()
-                    )
-                    Box(
-                        modifier = SafeModifier
-                            .weight(1f)
-                            .height(50.sdp)
-                            .padding(start = 8.sdp)
-                            .shimmerEffect()
-                    )
-                }
+
+    var adView by remember {
+        mutableStateOf<AdView?>(null)
+    }
+
+    val config = LocalScreenConfigs.current
+
+    DisposableEffect(key1 = owner) {
+        val observer = LifecycleEventObserver { _, event ->
+//            logError("state AD $event ${config.actualRouteName}")
+            if (event == Lifecycle.Event.ON_RESUME) {
+                adView?.resume()
+            } else if (event == Lifecycle.Event.ON_PAUSE) {
+                adView?.pause()
+            }
+        }
+        owner.lifecycle.addObserver(observer)
+        onDispose {
+            adView?.destroy()
+            owner.lifecycle.removeObserver(observer)
+        }
+    }
+    Box {
+        AndroidView(modifier = SafeModifier.fillMaxWidth(), factory = {
+            return@AndroidView AdView(it).also { view ->
+                adView = view
+                adFormatViewModel.loadBanner(
+                    config.route.replace("\\?.*".toRegex(), ""), view, usingCollapsible, owner
+                )
+            }
+        })
+        if (loaderStateCollection.state == DataResult.DataState.LOADING) {
+            Row {
+                Box(
+                    modifier = SafeModifier
+                        .size(50.sdp)
+                        .shimmerEffect()
+                )
+                Box(
+                    modifier = SafeModifier
+                        .weight(1f)
+                        .height(50.sdp)
+                        .padding(start = 8.sdp)
+                        .shimmerEffect()
+                )
             }
         }
     }
