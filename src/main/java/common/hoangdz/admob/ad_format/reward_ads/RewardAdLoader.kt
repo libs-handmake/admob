410211e2822aa362f479c9b8ce36851116416adc
Hoang <hoangdv@wavez.vn>
2024-07-03 17:38:22 +0700

    release 1.3.6

--- src/main/java/common/hoangdz/admob/ad_format/reward_ads/RewardAdLoader.kt
+++ src/main/java/common/hoangdz/admob/ad_format/reward_ads/RewardAdLoader.kt
@@ -2,6 +2,7 @@ package common.hoangdz.admob.ad_format.reward_ads
 
 import android.app.Activity
 import android.content.Context
+import android.os.CountDownTimer
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.rewarded.RewardedAd
@@ -9,6 +10,7 @@ import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import common.hoangdz.admob.ad_format.FullScreenAdsLoader
 import common.hoangdz.admob.ad_format.listener.AdLoaderListener
 import common.hoangdz.admob.config.ad_id.AdIds
+import common.hoangdz.lib.utils.ads.GlobalAdState
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -19,6 +21,8 @@ class RewardAdLoader @Inject constructor(
 ) : FullScreenAdsLoader<RewardedAd>() {
 
     var showOnLoaded = false
+
+    private var timer: CountDownTimer? = null
 
     override fun onTimeShowSaved() {
 
@@ -31,6 +35,20 @@ class RewardAdLoader @Inject constructor(
 
     override fun onLoad(activity: Activity?, adLoaderListener: AdLoaderListener?) {
         if (availableAd != null) return
+        timer?.cancel()
+        timer = object : CountDownTimer(
+            GlobalAdState.REWARD_LOADER_TIMEOUT, GlobalAdState.REWARD_LOADER_TIMEOUT
+        ) {
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                adLoaderListener?.onAdFailedToLoad()
+                showOnLoaded = false
+            }
+        }.start()
+
         RewardedAd.load(context,
             adIds.rewardID,
             AdRequest.Builder().setHttpTimeoutMillis(20_000).build(),
@@ -38,12 +56,15 @@ class RewardAdLoader @Inject constructor(
                 override fun onAdLoaded(p0: RewardedAd) {
                     onLoaded(p0)
                     if (showOnLoaded) show(activity, adLoaderListener)
+                    timer?.cancel()
                 }
 
                 override fun onAdFailedToLoad(p0: LoadAdError) {
                     onFailedToLoad(adLoaderListener)
+                    timer?.cancel()
                 }
             })
+
     }
 
     override fun onShow(
