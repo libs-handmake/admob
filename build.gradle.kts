90809a2a1468b81d1fc483b9d67b58096b0999c9
Hoang <hoangdv@wavez.vn>
2024-08-16 17:41:03 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,64 +1,19 @@
-import compose_config.composeImplementations
-import core_configs.coreAppImplementations
-import core_configs.jetpackComponentImplementation
-import firebase_configs.firebaseCoreImplementation
-
 plugins {
-    id(Plugins.ANDROID_LIBS)
-    kotlin("android")
-    id("kotlin-kapt")
-    id(Plugins.HILT)
-    id("maven-publish")
+    alias(libs.plugins.android.library)
+    id(libs.plugins.hoangdv.library.get().pluginId)
+    id(libs.plugins.hoangdv.core.get().pluginId)
+    id(libs.plugins.hoangdv.firebase.core.get().pluginId)
+    id(libs.plugins.hoangdv.jetpack.compose.get().pluginId)
 }
 
 android {
     namespace = "common.hoangdz.admob"
-    compileSdk = Configs.TARGET_SDK
-
-    defaultConfig {
-        minSdk = Configs.MIN_SUPPORTED_SDK
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = Configs.JAVA_TARGET
-        targetCompatibility = Configs.JAVA_TARGET
-    }
-    kotlinOptions {
-        jvmTarget = Configs.JVM_TARGET
-    }
-
-    buildFeatures {
-        dataBinding = true
-        viewBinding = true
-        compose = true
-        buildConfig = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = PluginsVer.COMPOSE_COMPILER
-    }
 }
 
 dependencies {
-    coreAppImplementations()
-    jetpackComponentImplementation()
-    composeImplementations()
-    firebaseCoreImplementation()
     implementation(project(":base:android-common"))
-    implementation(Deps.ADMOB)
-    implementation(Deps.SHIMMER_COMPOSE)
-    implementation(AdmobDeps.USER_MESSAGE_PLATFORM)
+    implementation(libs.play.services.ads)
+    implementation(libs.compose.shimmer)
+    implementation(libs.user.messaging.platform)
 }
 
-kapt {
-    correctErrorTypes = true
-}
